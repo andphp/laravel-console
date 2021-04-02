62b0a364f5ff078d435bd6d0618769c2abddaa87
daxiong <417170808@qq.com>
2021-04-03 01:49:48 +0800

    first commit

---
+++ src/CommandServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AndPHP\Console;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Created by PhpStorm.
+ * User: DaXiong
+ * Date: 2021/4/3
+ * Time: 1:23 AM
+ */
+class CommandServiceProvider extends ServiceProvider
+{
+
+    protected $commands = [];
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands = [
+            \AndPHP\Console\Commands\ModelCommand::class
+        ];
+
+        foreach ($this->commands as $command) {
+            $this->app->singleton($command, function ($command) {
+                return new $command();
+            });
+        }
+
+
+        $this->commands($this->commands);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return $this->commands;
+    }
+}
